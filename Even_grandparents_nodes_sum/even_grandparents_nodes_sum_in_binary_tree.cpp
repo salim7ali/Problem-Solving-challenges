5fb3c8403d6f8150446e4110c36979aad1905342
Salim <salim7ali@gmail.com>
2021-05-05 01:10:48 +0530

    added struct Node:

--- Even_grandparents_nodes_sum/even_grandparents_nodes_sum_in_binary_tree.cpp
+++ Even_grandparents_nodes_sum/even_grandparents_nodes_sum_in_binary_tree.cpp
@@ -1,6 +1,13 @@
 // https://www.youtube.com/watch?v=NcsZouBdd0c
+#include<bits/stdc++.h>
+using namespace std;
 
-void dfs(Node* node){
+struct Node{
+    Node *left, *right;
+    bool visited;
+};
+
+void dfs(Node* node, int &result_sum){
     node->visited = true;
 
     if(node->left->visited == false){
@@ -18,6 +25,7 @@ int get_even_grandparents_nodes_sum(Node* head){
 //   <parent, grandparent>
     pair<int, int> ancestors = {INT_MIN, INT_MIN};
 
-    
+    int result_sum =0;
+    dfs(head, result_sum);
 
 }

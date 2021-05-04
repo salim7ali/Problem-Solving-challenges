31c180aef644c467f55d4f531ec77518019b401d
Salim <salim7ali@gmail.com>
2021-05-05 01:18:44 +0530

    added logic for left and right child

--- Even_grandparents_nodes_sum/even_grandparents_nodes_sum_in_binary_tree.cpp
+++ Even_grandparents_nodes_sum/even_grandparents_nodes_sum_in_binary_tree.cpp
@@ -5,18 +5,29 @@ using namespace std;
 struct Node{
     Node *left, *right;
     bool visited;
+    int value;
 };
 
-void dfs(Node* node, int &result_sum){
+bool isEven(int num){
+    if(num%2)
+        return false;
+    return true;
+}
+
+void dfs(Node* node, pair<int, int> ancestors, int &result_sum){
     node->visited = true;
+
+    if(isEven(ancestors.second)){
+        result_sum += node->value;
+    }
 
     if(node->left->visited == false){
         node->left->visited = true;
-        dfs(node->left);
+        dfs(node->left, {node->value, ancestors.first}, result_sum);
     }
     if(node->right->visited == false){
         node->right->visited = true;
-        dfs(node->right);
+        dfs(node->right, {node->value, ancestors.first}, result_sum);
     }
 }
 
@@ -26,6 +37,6 @@ int get_even_grandparents_nodes_sum(Node* head){
     pair<int, int> ancestors = {INT_MIN, INT_MIN};
 
     int result_sum =0;
-    dfs(head, result_sum);
+    dfs(head, ancestors, result_sum);
 
 }

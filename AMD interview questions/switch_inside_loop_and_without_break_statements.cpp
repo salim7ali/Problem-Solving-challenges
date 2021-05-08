9718a274e06cedbae627080304c8a239d0ab9243
Salim <salim7ali@gmail.com>
2021-05-08 22:28:07 +0530

    added switch and loop logic

--- AMD interview questions/switch_inside_loop_and_without_break_statements.cpp
+++ AMD interview questions/switch_inside_loop_and_without_break_statements.cpp
@@ -4,5 +4,15 @@
 using namespace std;
 
 int main(){
-    
+    for(int i=0; i<20; i++){
+        switch(i){
+            case 1: i += 5;
+            case 2: i += 2;
+            case 3: i += 5;
+            default:
+            i += 4;
+            break;
+        }
+        cout<<i<<"\n";
+    }
 }

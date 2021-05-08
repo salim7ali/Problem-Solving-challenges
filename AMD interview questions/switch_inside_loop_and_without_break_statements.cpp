24685a1629612f44aa17fe111011b04101402381
Salim <salim7ali@gmail.com>
2021-05-08 22:37:07 +0530

    completed function

--- AMD interview questions/switch_inside_loop_and_without_break_statements.cpp
+++ AMD interview questions/switch_inside_loop_and_without_break_statements.cpp
@@ -6,9 +6,9 @@ using namespace std;
 void switchWithoutBreak(){
     for(int i=0; i<20; i++){
         switch(i){
-            case 1: i += 5;
-            case 2: i += 2;
-            case 3: i += 5;
+            case 0: i += 5;
+            case 1: i += 2;
+            case 2: i += 5;
             default:
             i += 4;
             break;

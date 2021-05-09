5e7a78b69a2e32d9112a7cec0d164d8774134e7c
Salim <salim7ali@gmail.com>
2021-05-09 11:45:04 +0530

    added closestPrime function

--- Closest Prime to given number/closest_prime_to_given_number.cpp
+++ Closest Prime to given number/closest_prime_to_given_number.cpp
@@ -4,24 +4,44 @@ using namespace std;
 
 class Prime{
     const MAX_PRIME =1000;
-    vector<bool> primeNos = vector<bool> (MAX_PRIME, true);
+    vector<bool> isPrime = vector<bool> (MAX_PRIME, true);
 public:
     Prime(){
-        primeNos[0] = false;
-        primeNos[1] = false;
+        isPrime[0] = false;
+        isPrime[1] = false;
         sieveOfEratosthenes();
     }
 
     void sieveOfEratosthenes(){
         for(int i=2; i<sqrt(MAX_PRIME-1); i++){
             for(int j=2; i*j<=MAX_PRIME; j++){
-                primeNos[i*j] = false;
+                isPrime[i*j] = false;
             }
         }    
     }
 
     int closestPrime(int num){
+        if(isPrime[num])
+            return num;
+        
+        // going left
+        for(int left=num-1; left>=2; left--){
+            if(isPrime[left])
+                break;
+        }
+        // going right
+        for(int right=num+1; right<MAX_PRIME; right++){
+            if(isPrime[right])
+                break;
+        }
 
+        // find whether left or right prime is closer
+        int left_dist = num-left;
+        int right_dist = right-num;
+        if(left_dist>right_dist)
+            return right;
+        else //left_dist<=right_dist
+            return left;
     }
 };
 

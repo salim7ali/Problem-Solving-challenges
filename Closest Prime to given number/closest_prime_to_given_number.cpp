c0372e63c8bec72d855bf05d6332bf96f8710513
Salim <salim7ali@gmail.com>
2021-05-09 11:23:27 +0530

    added prime generation function

--- Closest Prime to given number/closest_prime_to_given_number.cpp
+++ Closest Prime to given number/closest_prime_to_given_number.cpp
@@ -1,6 +1,30 @@
 #include<bits/stdc++.h>
+#include<algorithm>
 using namespace std;
 
+class Prime{
+    const MAX_PRIME =1000;
+    vector<bool> primeNos = vector<bool> (MAX_PRIME, true);
+public:
+    Prime(){
+        primeNos[0] = false;
+        primeNos[1] = false;
+        sieveOfEratosthenes();
+    }
+
+    void sieveOfEratosthenes(){
+        for(int i=2; i<sqrt(MAX_PRIME-1); i++){
+            for(int j=2; i*j<=MAX_PRIME; j++){
+                primeNos[i*j] = false;
+            }
+        }    
+    }
+
+    int closestPrime(int num){
+
+    }
+};
+
 int main(){
-    
+    Prime obj;
 }

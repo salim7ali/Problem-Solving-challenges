0cc8301cc2a2ce742ce4e9053a07803bf415585b
Salim <salim7ali@gmail.com>
2021-05-09 08:05:17 +0530

    function created

--- AMD interview questions/factorial_with_recursion.cpp
+++ AMD interview questions/factorial_with_recursion.cpp
@@ -1,7 +1,17 @@
-#include<bits/stdc++.h>
-
+// #include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
+int factorialWithRecursion(int n, vector<int> &solved_factorials){
+    if(solved_factorials[n] != -1)
+        return solved_factorials[n];
+    return n*factorialWithRecursion(n-1, solved_factorials);
+}
+
 int main(){
+    vector<int> solved_factorials(100, -1);
+    solved_factorials[0] = 1;   //  0! == 1
+    solved_factorials[1] = 1;   //  1! == 1
     
+    cout<<factorialWithRecursion(5, solved_factorials)<<"\n";
 }

e427d5b6bc10913bf7d3715cda928c93e109d494
Salim <salim7ali@gmail.com>
2021-05-09 08:12:47 +0530

    created class for function

--- AMD interview questions/factorial_with_recursion.cpp
+++ AMD interview questions/factorial_with_recursion.cpp
@@ -4,11 +4,17 @@
 #include<vector>
 using namespace std;
 
-int factorialWithRecursion(int n, vector<int> &solved_factorials){
-    if(solved_factorials[n] != -1)
-        return solved_factorials[n];
-    return n*factorialWithRecursion(n-1, solved_factorials);
-}
+
+
+class Factorial{
+
+public:
+    int factorialWithRecursion(int n, vector<int> &solved_factorials){
+        if(solved_factorials[n] != -1)
+            return solved_factorials[n];
+        return n*factorialWithRecursion(n-1, solved_factorials);
+    }
+};
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -17,9 +23,11 @@ int main(){
     vector<int> solved_factorials(100, -1);
     solved_factorials[0] = 1;   //  0! == 1
     solved_factorials[1] = 1;   //  1! == 1
+
+    Factorial obj;
     
-    std::cout<<factorialWithRecursion(0, solved_factorials)<<"\n";
-    std::cout<<factorialWithRecursion(1, solved_factorials)<<"\n";
-    std::cout<<factorialWithRecursion(3, solved_factorials)<<"\n";
-    std::cout<<factorialWithRecursion(5, solved_factorials)<<"\n";
+    std::cout<<obj.factorialWithRecursion(0, solved_factorials)<<"\n";
+    std::cout<<obj.factorialWithRecursion(1, solved_factorials)<<"\n";
+    std::cout<<obj.factorialWithRecursion(3, solved_factorials)<<"\n";
+    std::cout<<obj.factorialWithRecursion(5, solved_factorials)<<"\n";
 }

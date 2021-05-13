d4439ffde40080a2463ed36a31fab5bfec9cdada
Salim <salim7ali@gmail.com>
2021-05-13 11:15:00 +0530

    created generator class

---
+++ Python Generators/generator_code.py
@@ -0,0 +1,18 @@
+class Gen:
+    def __init__(self, number):
+        self.number=number
+        self.prev=0
+
+    def __next__():
+        return self.next()
+        
+    def next():
+        if(self.prev == self.number):
+            raise System()
+
+        rv = self.prev**2
+        self.prev += 1
+
+        return rv
+
+

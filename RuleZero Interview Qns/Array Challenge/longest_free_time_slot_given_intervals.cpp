04a3978eec77eb0992739caaa36af9e10bec207a
Salim <salim7ali@gmail.com>
2021-05-15 08:32:02 +0530

    _

--- RuleZero Interview Qns/Array Challenge/longest_free_time_slot_given_intervals.cpp
+++ RuleZero Interview Qns/Array Challenge/longest_free_time_slot_given_intervals.cpp
@@ -3,4 +3,3 @@
 // For example, strArr may be ["10:00AM-12:30PM", "02:00AM-02:45PM", "09:10AM-09:50AM"]. Your program will have to output the longest
 // amount of free time available between the start of your first event and the end of your last event in the format hh:mm. The
 // start event should be the earliest event in the day and the latest event should be the latest event in the day. The __GCC_ASM_FLAG_OUTPUTS__for the 
-
---
+++ RuleZero Interview Qns/Array Challenge/longest_free_time_slot_given_intervals.py
@@ -0,0 +1,14 @@
+# Have the function ArrayChallenge(strArr) read the strArr parameter being passed which will represent a full day 
+# and will be filled with events that span from time X to time Y in the day. The format of each event will be hh:mmAM/PM-hh:mmAM/PM.
+# For example, strArr may be ["10:00AM-12:30PM", "02:00AM-02:45PM", "09:10AM-09:50AM"]. Your program will have to output the longest
+# amount of free time available between the start of your first event and the end of your last event in the format hh:mm. The
+# start event should be the earliest event in the day and the latest event should be the latest event in the day. The __GCC_ASM_FLAG_OUTPUTS__for the 
+
+class Schedule:
+    def __init__(self, booked_slots):
+        self.booked_slots = booked_slots   
+
+
+if __name__ == "__main__":
+    employee1 = Schedule(["10:00AM-12:30PM", "02:00AM-02:45PM", "09:10AM-09:50AM"])
+    employee1.ArrayChallenge()

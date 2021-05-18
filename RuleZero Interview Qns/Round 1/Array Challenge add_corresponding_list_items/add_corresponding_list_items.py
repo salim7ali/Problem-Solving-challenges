6b5d6e0db11492d423154850d4dabe17bac896e6
Salim <salim7ali@gmail.com>
2021-05-18 08:37:19 +0530

    input converted to a list

--- RuleZero Interview Qns/Round 1/Array Challenge add_corresponding_list_items/add_corresponding_list_items.py
+++ RuleZero Interview Qns/Round 1/Array Challenge add_corresponding_list_items/add_corresponding_list_items.py
@@ -1,7 +1,21 @@
-def ArrayChallenge():
+
 
 class List_Handler:
-    def __init__(self):
-        
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    def ArrayChallenge(self):
+        self.first = [itm for itm in self.first.split(',')]
+        self.second = [itm for itm in self.second.split(',')]
+        print(first, second, sep=" ")
+
+
 
 if __name__ == "__main__":
+    first = "[5, 2, 3]"
+    second = "[2, 2, 3, 10, 6]"
+
+    obj = List_Handler(first, second)
+    obj.ArrayChallenge()
+    

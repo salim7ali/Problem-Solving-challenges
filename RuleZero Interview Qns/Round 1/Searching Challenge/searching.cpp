f7d7a00f391ca27a4cba739b81b53f9946dc6e96
Salim <salim7ali@gmail.com>
2021-05-19 16:19:24 +0530

    functionaliy completed

--- RuleZero Interview Qns/Round 1/Searching Challenge/searching.cpp
+++ RuleZero Interview Qns/Round 1/Searching Challenge/searching.cpp
@@ -2,9 +2,30 @@
 using namespace std;
 
 class Searching{
+    Searching(string str){
+        this.str = str;
+    }
+    string SearchingChallenge(){
+        stack<char> St;
 
-};
+        for(int i=0; i<this.str.length(); i++){
+            if(this.str[i] == '(')
+                St.push('(');
+            if(this.str[i] == ')'){
+                if(St.empty()==false)
+                    St.pop();
+                else 
+                    return "0"; 
+            }
+        }
+
+        if(St.empty == true)
+            return "1";
+        return "0";
+    }
+}; 
 
 int main(){
-    
+    Searching obj("(coder)(byte))");
+    cout<<obj.SearchingChallenge()<<"\n";
 }

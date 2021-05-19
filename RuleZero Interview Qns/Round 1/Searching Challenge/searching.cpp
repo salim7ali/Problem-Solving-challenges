bd385dae522b658b645e2081d536a974b3c100c8
Salim <salim7ali@gmail.com>
2021-05-19 16:27:44 +0530

    solved

--- RuleZero Interview Qns/Round 1/Searching Challenge/searching.cpp
+++ RuleZero Interview Qns/Round 1/Searching Challenge/searching.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 
 class Searching{
+    // instance variable
+    string str;
+public:
     Searching(string str){
-        this.str = str;
+        this->str = str;
     }
     string SearchingChallenge(){
         stack<char> St;
 
-        for(int i=0; i<this.str.length(); i++){
-            if(this.str[i] == '(')
+        for(int i=0; i<this->str.length(); i++){
+            if(this->str[i] == '(')
                 St.push('(');
-            if(this.str[i] == ')'){
+            if(this->str[i] == ')'){
                 if(St.empty()==false)
                     St.pop();
                 else 
@@ -19,13 +22,16 @@ class Searching{
             }
         }
 
-        if(St.empty == true)
+        if(St.empty() == true)
             return "1";
         return "0";
     }
 }; 
 
 int main(){
-    Searching obj("(coder)(byte))");
-    cout<<obj.SearchingChallenge()<<"\n";
+    Searching obj1("(coder)(byte))");
+    cout<<obj1.SearchingChallenge()<<"\n";
+
+    Searching obj2("(c(oder))b(yte)");
+    cout<<obj2.SearchingChallenge()<<"\n";
 }

c5703813e7831eae2c78a7ebbab395b44d19af41
Salim <salim7ali@gmail.com>
2021-05-24 23:07:01 +0530

    Handled edge cases. Also added relevant test cases for the same.

--- Replace 0 with 5 in an int without use of other data types/replace_0_with_5.cpp
+++ Replace 0 with 5 in an int without use of other data types/replace_0_with_5.cpp
@@ -10,32 +10,46 @@ public:
         NUM=num;
     }
 
-    int replace_method(){
-        int num=NUM;
+    int replace_zero(){
+        if(NUM==0)
+            return 5;
+        
+        int num=abs(NUM);
         int result_num =0;
+
 
         int position =0;
         while(num){
             int lsd = num%10;
             num /= 10;
 
-            if(lsd==0){
+            if(lsd==0)
                 result_num += pow(10, position)*5;
-            }else{
+            else
                 result_num += pow(10, position)*lsd;
-            }
             position += 1; 
         }
+        if(NUM<0)
+            result_num *= -1;
+
         return result_num;
     }
-
 };
 
 int main(){
   Replacer obj1(102);
-  cout<<obj1.replace_method()<<"\n";
+  cout<<obj1.replace_zero()<<"\n";
 
   Replacer obj2(1020);
-  cout<<obj2.replace_method()<<"\n";
+  cout<<obj2.replace_zero()<<"\n";
+
+  Replacer obj3(-503210);
+  cout<<obj3.replace_zero()<<"\n";
+
+  Replacer obj4(0);
+  cout<<obj4.replace_zero()<<"\n";
+
+  Replacer obj5(-1);
+  cout<<obj5.replace_zero()<<"\n";
     
 }

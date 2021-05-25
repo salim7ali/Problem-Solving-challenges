21388adb19f43c373789a84d9df132924fbbec6a
Salim <salim7ali@gmail.com>
2021-05-25 18:20:25 +0530

    Added boiler plate code.

---
+++ Interview.io/river_crossing_over_rocks/solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class RiverCrossing{
+    // instance variables
+    int rocks;
+private:
+    RiverCrossing(int rocks){
+        this->rocks = rocks;
+    }
+    int get_no_of_ways(){
+
+    }
+};
+
+int main(){{
+     RiverCrossing obj1;
+     cout<<obj1.get_no_of_ways()<<"\n";
+}

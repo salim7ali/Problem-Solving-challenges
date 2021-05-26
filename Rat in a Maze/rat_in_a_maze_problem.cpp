6a1f423d7b125e2c18e3511dc2b45d0af1959b83
Salim <salim7ali@gmail.com>
2021-05-26 21:14:47 +0530

    Added class instance variables, added constructor and created isValidNode function.

--- Rat in a Maze/rat_in_a_maze_problem.cpp
+++ Rat in a Maze/rat_in_a_maze_problem.cpp
@@ -1,10 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Maze{
+#define v2d vector<vector<int>>
+
+
+class MazeSolver{
+    v2d maze;
+    v2d currently_visited;
+
+    pair<int, int> START_INDICES = {0, 0};
+    pair<int, int> END_INDICES;
+    
+    vector<pair<int, int>> directions= {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+public:
+    MazeSolver(v2d &maze){
+        this->maze = maze;
+
+        this->END_INDICES = {maze.size()-1, maze[0].size()-1};
+
+        this->currently_visited = maze;
+        for(int i=0; i<this->currently_visited.size(); i++)
+            for(int j=0; j<this->currently_visited[0].size(); j++)
+                this->currently_visited[i][j] =0;
+    }
+
+    bool isValidNode(int i, int j, int rows, int cols){
+        if(i>=0 && i<rows && j>=0 && j<cols)
+            return true;
+        return false;
+    }
+
+    int dfs_path(){
+        
+    }
 
 };
 
 int main(){
-  Maze obj;
+    v2d grid = {{1, 1, 1},
+                                {1, 1, 0},
+                                {1, 1, 1}};
+    MazeSolver obj(grid);
+    obj.dfs_path();
+
 }

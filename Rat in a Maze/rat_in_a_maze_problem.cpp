f35080b99f734352deef7df24bbebd683bce849d
Salim <salim7ali@gmail.com>
2021-05-26 21:38:40 +0530

    Completed logic and functions. Need to test the implementation.

--- Rat in a Maze/rat_in_a_maze_problem.cpp
+++ Rat in a Maze/rat_in_a_maze_problem.cpp
@@ -24,23 +24,50 @@ public:
                 this->currently_visited[i][j] =0;
     }
 
-    bool isValidNode(int i, int j, int rows, int cols){
-        if(i>=0 && i<rows && j>=0 && j<cols)
+    bool isValidNode(int i, int j, v2d &maze){
+        if(i>=0 && i<maze.size() && j>=0 && j<maze[0].size)
             return true;
         return false;
     }
 
-    int dfs_path(){
+    void disp_matrix(v2d &matrix){
+        for(int i=0; i< matrix.size(); i++){
+            for(int j=0; j< matrix[0].size(); j++){
+                cout<<matrix[i][j]<<" ";
+            }cout<<"\n";
+        }
+    }
+
+    void dfs(int i, int j){
+        currently_visited[i][j] = 1;
+        if(i==END_INDICES.first && j==END_INDICES.second){
+            disp_matrix(currently_visited);
+            currently_visited[i][j] = 0;
+            return;
+        }
+
+        for(int k=0; k<4; k++){
+            int neighbour_i = i + directions[k].first; 
+            int neighbour_j = j + directions[k].second; 
+            if(isValidNode(neighbour_i, neighbour_j, maze) && currently_visited[neighbour_i][neighbour_j]==0){
+                dfs(neighbour_i, neighbour_j);
+            }
+        }
+        currently_visited[i][j] = 0;
+    }
+
+    int generate_paths(){
         
+        dfs(START_INDICES.first, START_INDICES.second);
     }
 
 };
 
 int main(){
     v2d grid = {{1, 1, 1},
-                                {1, 1, 0},
-                                {1, 1, 1}};
+                {1, 1, 0},
+                {1, 1, 1}};
     MazeSolver obj(grid);
-    obj.dfs_path();
+    obj.generate_paths();
 
 }

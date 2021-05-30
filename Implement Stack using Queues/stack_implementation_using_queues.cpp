601d4ab83f342cb19cd8ad5be959d16a8c31cc92
Salim <salim7ali@gmail.com>
2021-05-30 18:28:05 +0530

    First commit. Added boiler plate code.

---
+++ Implement Stack using Queues/stack_implementation_using_queues.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class StackHandler{
+    queue<int> first_Q;
+    queue<int> second_Q;
+public:
+    void stack_push(int num){
+
+    }
+
+    int stack_pop(){
+        
+    }
+}
+
+int main(){
+    
+}

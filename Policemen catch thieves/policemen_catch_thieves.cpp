a75ce6c4e77578dc8ce0757fa8c252529eb2b0ea
Salim <salim7ali@gmail.com>
2021-06-02 21:34:06 +0530

    First commit. Added boiler plate code.

---
+++ Policemen catch thieves/policemen_catch_thieves.cpp
@@ -0,0 +1,14 @@
+// https://www.youtube.com/watch?v=MFkqnGiCCiU&t=1703s
+// https://www.geeksforgeeks.org/policemen-catch-thieves/
+
+#include<bits/stdc++.h>
+using namespace std; 
+
+class CatchThieves{
+public:
+
+}
+
+int main(){
+    CatchThieves obj;    
+}

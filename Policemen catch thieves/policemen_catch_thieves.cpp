77b1d1870793a956794829e28e12525d5cd57ca3
Salim <salim7ali@gmail.com>
2021-06-02 22:09:12 +0530

    findNext() added.

--- Policemen catch thieves/policemen_catch_thieves.cpp
+++ Policemen catch thieves/policemen_catch_thieves.cpp
@@ -13,8 +13,56 @@ public:
         this->k = k;
     }
 
-    int maxThievesCaught(){
+    void findNext(int &currIndex, char thief_of_police, vector<char> &police_thief){
+        int i; 
+        if(thief_of_police == 'P'){
+            for(i=currIndex+1; i<police_thief.size(); i++){
+                if(police_thief[i] == 'P'){
+                    currIndex = i;
+                    return;
+                }
+            }
+            currIndex = INT_MAX;
+        }else if(thief_of_police == 'T'){
+            for(i=currIndex+1; i<police_thief.size(); i++){
+                if(police_thief[i] == 'T'){
+                    currIndex = i;
+                    return;
+                }
+            }
+            currIndex = INT_MAX;
+        }
+    }
 
+    int maxThievesCaught(){
+        int policeIndex = INT_MIN;
+        int thiefIndex = INT_MIN;
+
+        // finding first instances of police and thief from left side
+        for(int i=0; i<police_thief.size(); i++){
+            if(policeIndex != INT_MIN && thiefIndex != INT_MIN)
+                break;
+            if(police_thief[i] == 'P' && policeIndex == INT_MIN)
+                policeIndex = i;
+            if(police_thief[i] == 'T' && thiefIndex == INT_MIN)
+                thiefIndex = i;
+        }
+
+        int maxCaught =0;
+        while(policeIndex < police_thief.size() && thiefIndex < police_thief.size()){
+            if(abs(policeIndex-thiefIndex) <= k){
+                maxCaught += 1;
+                findNext(policeIndex, 'P', police_thief);
+                findNext(thiefIndex, 'T', police_thief);
+            }else{
+                if(policeIndex < thiefIndex)
+                    findNext(policeIndex, 'P', police_thief);
+                else  //policeIndex > thiefIndex
+                    findNext(thiefIndex, 'T', police_thief);
+            }
+        }
+
+        
     }
 }
 

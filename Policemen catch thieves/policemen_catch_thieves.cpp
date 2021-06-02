d30197fe785696683072d12f60d86b19099dadae
Salim <salim7ali@gmail.com>
2021-06-02 21:41:04 +0530

    Added constructor and function signatrures.

--- Policemen catch thieves/policemen_catch_thieves.cpp
+++ Policemen catch thieves/policemen_catch_thieves.cpp
@@ -5,10 +5,31 @@
 using namespace std; 
 
 class CatchThieves{
+    vector<char> police_thief;
+    int k;
 public:
+    CatchThieves(vector<char> police_thief, int k){
+        this->police_thief = police_thief;
+        this->k = k;
+    }
 
+    int maxThievesCaught(){
+
+    }
 }
 
 int main(){
-    CatchThieves obj;    
+    vector<char> police_thief;
+
+    police_thief = {'P', 'T', 'T', 'P', 'T'};
+    CatchThieves obj1(police_thief, 1);    
+    cout<<obj1.maxThievesCaught()<<"\n";
+    
+    police_thief = {'T', 'T', 'P', 'P', 'T', 'P'};
+    CatchThieves obj2(police_thief, 2);    
+    cout<<obj2.maxThievesCaught()<<"\n";
+
+    police_thief = {'P', 'T', 'P', 'T', 'T', 'P'};
+    CatchThieves obj3(police_thief, 3);    
+    cout<<obj3.maxThievesCaught()<<"\n";
 }

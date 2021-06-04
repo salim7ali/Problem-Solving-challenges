bd2bc8576e85e1491e46e53154ef50aeca6493d4
Salim <salim7ali@gmail.com>
2021-06-04 21:48:37 +0530

    First commit. Added boiler plate code.

---
+++ Geeks for Geeks/Search_element_in_increasing_decreasing_sequence/Search_element_in_increasing_decreasing_sequence.cpp
@@ -0,0 +1,20 @@
+// https://www.geeksforgeeks.org/amazon-interview-experience-set-394-campus-full-time/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class IncDecList{
+    vector<int> inc_dec_list;
+public: 
+    IncDecList(vector<int> inc_dec_list){
+        this->inc_dec_list = inc_dec_list; 
+    }
+
+    int get_element_index(int ele){
+
+    }
+};
+
+int main(){
+    IncDecList obj({1, 4, 23, 45, 50, 49, 48, 40, 21, 4, 2});
+}

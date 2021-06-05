1bb5018756144a0e72a185f2bb88635194cec62b
Salim <salim7ali@gmail.com>
2021-06-05 13:25:55 +0530

    Added bin_search().

--- Geeks for Geeks/Search_element_in_increasing_decreasing_sequence/Search_element_in_increasing_decreasing_sequence.cpp
+++ Geeks for Geeks/Search_element_in_increasing_decreasing_sequence/Search_element_in_increasing_decreasing_sequence.cpp
@@ -33,11 +33,34 @@ public:
         }
     }
 
+    int bin_search(int L, int R, int ele){
+        while(L<=R){
+            int mid_index = L + floor((R-L)/2);
+            
+            if(inc_dec_list[mid_index] == ele){
+                return mid_index;
+            }else if(ele < inc_dec_list[mid_index]){
+                R = mid_index-1;
+            }else{ //inc_dec_list[mid_index] < ele
+                L = mid_index+1;
+            }
+        }
+        return INT_MIN;
+    }
+
     int get_element_index(int ele){
         int L = 0;
         int R = inc_dec_list.size()-1;
 
-        cout<<get_peak_index(L, R);
+        int peak_index = get_peak_index(L, R);
+
+        int ele_index;
+        if(inc_dec_list[0]<=ele && ele<inc_dec_list[peak_index]){
+            ele_index = bin_search(0, peak_index-1, ele);
+        }else if(inc_dec_list[peak_index]<=ele && ele<=inc_dec_list[inc_dec_list.size()-1]){
+            // ele_index = rev_bin_search(peak_index, inc_dec_list.size()-1, ele);
+        }else
+            return INT_MIN;
     }
 };
 

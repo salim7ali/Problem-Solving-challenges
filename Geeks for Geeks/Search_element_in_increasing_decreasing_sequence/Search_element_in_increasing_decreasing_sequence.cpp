9001e14d7b9d89ad5574b06e877854017209f3e5
Salim <salim7ali@gmail.com>
2021-06-05 13:08:12 +0530

    Added peak finder function.

--- Geeks for Geeks/Search_element_in_increasing_decreasing_sequence/Search_element_in_increasing_decreasing_sequence.cpp
+++ Geeks for Geeks/Search_element_in_increasing_decreasing_sequence/Search_element_in_increasing_decreasing_sequence.cpp
@@ -10,11 +10,36 @@ public:
         this->inc_dec_list = inc_dec_list; 
     }
 
-    int get_element_index(int ele){
+    int get_peak_index(int L, int R){
+        while(L<=R){
+            int mid_index = L + floor((R-L)/2);
+            
+            
+            if(inc_dec_list[mid_index-1] < inc_dec_list[mid_index] 
+            && inc_dec_list[mid_index] > inc_dec_list[mid_index+1]){
+                return mid_index;
+            }
+            else if(inc_dec_list[mid_index-1] < inc_dec_list[mid_index] 
+            && inc_dec_list[mid_index] < inc_dec_list[mid_index+1]){ // mid on increasing side
+                L = mid_index + 1;
+            }
+            else if(inc_dec_list[mid_index-1] > inc_dec_list[mid_index] 
+            && inc_dec_list[mid_index] > inc_dec_list[mid_index+1]){  // mid on decreasing side
+                R = mid_index -1;
+            }
+        }
 
+    }
+
+    int get_element_index(int ele){
+        int L = 0;
+        int R = inc_dec_list.size()-1;
+
+        cout<<get_peak_index(L, R);
     }
 };
 
 int main(){
     IncDecList obj({1, 4, 23, 45, 50, 49, 48, 40, 21, 4, 2});
+    obj.get_element_index(23);
 }

05bc6cef9e6cc709c2403f3ac6f10f9f940e3afe
Salim <salim7ali@gmail.com>
2021-06-10 20:56:33 +0530

    Created dfs function and modified removeIslands function.

--- Remove Islands not connected to edges/remove_Islands_not_connected_to_edges.cpp
+++ Remove Islands not connected to edges/remove_Islands_not_connected_to_edges.cpp
@@ -1,12 +1,40 @@
 // https://www.youtube.com/watch?v=4tYoVx0QoN0&t=158s
+// Remove all black pixels not connected to border of image
 
 #include<bits/stdc++.h>
 using namespace std;
 
+// 1 -> black
+// 0 -> white
 class Islands{
 public:
-    void removeIslands(vector<vector<int>> matrix){
+    void dfs(int i, int j, vector<vector<int>> &matrix, vector<vector<bool>> &visited){
+        visited[i][j] = true;
+        
+        int neighbours[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        for(int )
+        if(visited[i][j] )
+    }
 
+    vector<vector<int>> removeIslands(vector<vector<int>> matrix){
+        vector<vector<bool>> visited(matrix.size(), vector<int> (matrix[0].size(), false));
+        
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[0].size(); j++){
+                int first_row = 0;
+                int last_row = matrix.size()-1;
+                int first_col = 0;
+                int last_col = matrix[0].size()-1;
+                
+                if((i==first_row || i==last_row || j==first_col || j==last_col) && matrix[i][j]==1){
+                    dfs(i, j, matrix, visited);
+                }
+            }
+        }
+
+
+
+        return matrix;
     }
 };
 
@@ -21,5 +49,5 @@ int main(){
     };
     
     Islands obj;
-    obj.removeIslands(matrix);
+    vector<vector<int>> resultMatrix = obj.removeIslands(matrix);
 }

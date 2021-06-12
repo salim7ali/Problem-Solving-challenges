bd71abbcdb14debe8d166e00d1ac2a486d4feff0
Salim <salim7ali@gmail.com>
2021-06-12 20:46:24 +0530

    Logic complete. O(n^2) approach.

--- Nokia Colligate Code Warriors Hunt 2021/Set 1/product_of_pairs.cpp
+++ Nokia Colligate Code Warriors Hunt 2021/Set 1/product_of_pairs.cpp
@@ -1,6 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+long long solve (int N, int K, vector<int> A) {
+   // Write your code here
+
+   long long result = 0;
+   for(int i=0; i<A.size(); i++){
+       for(int j=i+1; j<A.size(); j++){
+           if(i<j && ((j-i)%k==0)){
+               result += A[i]*A[j];
+           }
+       }
+   }
+
+   return result;
+}
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    for(int t_i = 0; t_i < T; t_i++)
+    {
+        int N;
+        cin >> N;
+        int K;
+        cin >> K;
+        vector<int> A(N);
+        for(int i_A = 0; i_A < N; i_A++)
+        {
+        	cin >> A[i_A];
+        }
+
+        long long out_;
+        out_ = solve(N, K, A);
+        cout << out_;
+        cout << "\n";
+    }
 }

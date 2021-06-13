3414535a513318a7069912382174da963e4e48cc
Salim <salim7ali@gmail.com>
2021-06-13 23:13:17 +0530

    Brute force solution O(n^2).

---
+++ Nokia Colligate Code Warriors Hunt 2021/Set 2/Perfect Triplets/perfect_triplets.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPerfectSquare(long long num){
+    // num should always be long long. Sometimes int might get passed
+
+    long long sqrtNum = sqrt(num);
+    // cout<<num<<" "<<sqrtNum<<" "<<sqrtNum*sqrtNum<<"\n";
+    if(sqrtNum*sqrtNum == num)
+        return true;
+    return false;
+}
+
+long long countTriplets(int N, vector<int> A){
+    long long count =0;
+    
+    for(int i=0; i<A.size(); i++){
+        for(int j=0; j<A.size(); j++){
+            for(int k=0; k<A.size(); k++){
+                long long prod = (long long)A[i]*(long long)A[j]*(long long)A[k];
+                if(isPerfectSquare(prod)){
+                    count += 1;   
+                }
+            }
+        }
+    }
+    return count;
+}
+
+int main(){
+    vector<int> nums = {3, 6, 2};
+    cout<<countTriplets(3, nums)<<"\n";
+    
+    vector<int> nums = {3, 6, 2};
+    cout<<countTriplets(3, nums)<<"\n";
+}

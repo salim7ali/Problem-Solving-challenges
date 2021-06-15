04f8a62465aaddf5f7c62b0ac617817501940d82
Salim <salim7ali@gmail.com>
2021-06-15 22:27:54 +0530

    First commit. Added boiler plate code.

---
+++ Geeks for Geeks/Smallest of three integers without comparison operators/smallest_of_three_without_comparison.cpp
@@ -0,0 +1,15 @@
+// https://www.geeksforgeeks.org/smallest-of-three-integers-without-comparison-operators/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Comparison{
+public:
+    int smallestOfThree(int a, int b, int c){
+
+    }
+}
+
+int main(){
+    Comparison obj;
+    obj.smallestOfThree(5, 15, 4);
+}

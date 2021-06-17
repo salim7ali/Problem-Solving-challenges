8600942a6a1cc4cbb6bf78d6b9e1aef3276078ff
Salim <salim7ali@gmail.com>
2021-06-17 23:08:53 +0530

    Completed the rotated matrix.

--- Geeks for Geeks/Rotate 2d array clockwise and anti clock wise 90 degrees/rotate_90.cpp
+++ Geeks for Geeks/Rotate 2d array clockwise and anti clock wise 90 degrees/rotate_90.cpp
@@ -6,8 +6,21 @@ using namespace std;
 
 class Matrix{
 public:
-    vector<vector<int>> rotateClockwise90(){
+    vector<vector<int>> rotateClockwise90(vector<vector<int>> matrix){
+        // switching for rotated matrix
+        int rotated_row = matrix[0].size();
+        int rotated_col = matrix.size();
 
+        vector<vector<int>> rotated_matrix;
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[0].size(); j++){
+                int rotated_curr_row = matrix[0].size() - j -1;
+                int rotated_curr_col = matrix.size() - i -1; 
+
+                rotated_matrix[rotated_curr_row][rotated_curr_col] = matrix[i][j];
+            }
+        }
+        return rotated_matrix;
     }
 
     vector<vector<int>> rotateAntiClockwise90(){
@@ -15,14 +28,15 @@ public:
     }
 
     void dispMatrix(vector<vector<int>> matrix){
-        
+
     }
 }
 
 int main(){
     
+    vector<vector<int>> matrix;
     
-    Matrix obj();
+    Matrix obj(matrix);
     vector<vector<int>> result;
     result = obj.rotateAntiClockwise90();
     obj.rotateAntiClockwise90();

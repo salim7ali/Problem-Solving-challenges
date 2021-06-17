8c47931dc5ce12186b6343fdac9e22e09f98920b
Salim <salim7ali@gmail.com>
2021-06-17 23:31:43 +0530

    Solved bug issues.

--- Geeks for Geeks/Rotate 2d array clockwise and anti clock wise 90 degrees/rotate_90.cpp
+++ Geeks for Geeks/Rotate 2d array clockwise and anti clock wise 90 degrees/rotate_90.cpp
@@ -6,38 +6,59 @@ using namespace std;
 
 class Matrix{
 public:
-    vector<vector<int>> rotateClockwise90(vector<vector<int>> matrix){
+    vector<vector<int>> matrix;
+    Matrix(vector<vector<int>> matrix){
+        this->matrix = matrix;
+    }
+
+    vector<vector<int>> rotateClockwise90(){
         // switching for rotated matrix
-        int rotated_row = matrix[0].size();
-        int rotated_col = matrix.size();
+        int rotated_row_size = this->matrix[0].size();
+        int rotated_col_size = this->matrix.size();
 
-        vector<vector<int>> rotated_matrix;
-        for(int i=0; i<matrix.size(); i++){
-            for(int j=0; j<matrix[0].size(); j++){
-                int rotated_curr_row = matrix[0].size() - j -1;
-                int rotated_curr_col = matrix.size() - i -1; 
+        vector<vector<int>> rotated_matrix(rotated_row_size, vector<int> (rotated_col_size));
+        for(int i=0; i<this->matrix.size(); i++){
+            int rotated_curr_col = rotated_col_size - i -1; 
+            for(int j=0; j<this->matrix[0].size(); j++){
+                int rotated_curr_row = j;
 
-                rotated_matrix[rotated_curr_row][rotated_curr_col] = matrix[i][j];
+                rotated_matrix[rotated_curr_row][rotated_curr_col] = this->matrix[i][j];
             }
         }
         return rotated_matrix;
     }
 
-    vector<vector<int>> rotateAntiClockwise90(){
+    // vector<vector<int>> rotateAntiClockwise90(){
 
-    }
+    // }
 
     void dispMatrix(vector<vector<int>> matrix){
-
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[0].size(); j++){
+                cout<<matrix[i][j]<<" ";
+            }cout<<"\n";
+        }cout<<"\n";
     }
-}
+};
 
 int main(){
     
-    vector<vector<int>> matrix;
+    vector<vector<int>> matrix ={
+        {1, 2, 3, 4, 5},
+        {6, 7, 8, 9, 10},
+        {11, 12, 13, 14, 15},
+    };
     
     Matrix obj(matrix);
     vector<vector<int>> result;
-    result = obj.rotateAntiClockwise90();
-    obj.rotateAntiClockwise90();
+    
+    obj.dispMatrix(matrix);
+    result = obj.rotateClockwise90();
+    obj.dispMatrix(result);
+    // obj.rotateAntiClockwise90();
+
+    // obj.dispMatrix(obj.matrix);
+    
+
+    return 0;
 }

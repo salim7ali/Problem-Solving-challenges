e6c4c402ffbb3bd2a0a99dc04bbdaeb7f5c285e9
Salim <salim7ali@gmail.com>
2021-06-17 22:59:11 +0530

    First commit. Added boiler plate code.

---
+++ Geeks for Geeks/Rotate 2d array clockwise and anti clock wise 90 degrees/rotate_90.cpp
@@ -0,0 +1,29 @@
+// https://www.geeksforgeeks.org/microsoft-interview-experience-8-years-experienced/
+// Rotate 2d array clockwise and anti clock wise 90 degrees.2d array need not be square
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Matrix{
+public:
+    vector<vector<int>> rotateClockwise90(){
+
+    }
+
+    vector<vector<int>> rotateAntiClockwise90(){
+
+    }
+
+    void dispMatrix(vector<vector<int>> matrix){
+        
+    }
+}
+
+int main(){
+    
+    
+    Matrix obj();
+    vector<vector<int>> result;
+    result = obj.rotateAntiClockwise90();
+    obj.rotateAntiClockwise90();
+}

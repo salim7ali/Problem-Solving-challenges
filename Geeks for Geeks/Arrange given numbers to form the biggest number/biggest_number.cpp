d7b97b14e20d2284d77e3bfbc2b934e0f010765e
Salim <salim7ali@gmail.com>
2021-06-18 22:33:00 +0530

    Function Logic completed.

--- Geeks for Geeks/Arrange given numbers to form the biggest number/biggest_number.cpp
+++ Geeks for Geeks/Arrange given numbers to form the biggest number/biggest_number.cpp
@@ -7,7 +7,21 @@ using namespace std;
 class Number{
 public:
     vector<int> getLargestNumber(vector<int> nums){
+        for(int i=0; i<nums.size()-1; i++){
+            for(int j=i+1; j<nums.size(); j++){
+                string str_A = to_string(nums[i]);
+                string str_B = to_string(nums[j]);
 
+                string str_A_B = str_A+str_B;
+                string str_B_A = str_B+str_A;
+
+                int A_B = stoi(str_A_B);
+                int B_A = stoi(str_B_A);
+
+                if(B_A > A_B){}
+                    swap(nums[i], nums[j]);
+            }
+        }
     }
 };
 

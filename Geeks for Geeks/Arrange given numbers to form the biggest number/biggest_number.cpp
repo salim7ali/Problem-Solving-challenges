dac665e2c20f8ba9a072eee42f231c51928db02e
Salim <salim7ali@gmail.com>
2021-06-18 22:23:51 +0530

    First commit. Added boiler plate code.

---
+++ Geeks for Geeks/Arrange given numbers to form the biggest number/biggest_number.cpp
@@ -0,0 +1,26 @@
+// https://www.geeksforgeeks.org/given-an-array-of-numbers-arrange-the-numbers-to-form-the-biggest-number/
+// https://www.youtube.com/watch?v=qEIGhVtZ-sg
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Number{
+public:
+    vector<int> getLargestNumber(vector<int> nums){
+
+    }
+};
+
+void dispVector(vector<int> nums){
+    for(auto num: nums){
+        cout<<num<<" ";
+    }cout<<"\n";
+}
+
+int main(){
+    vector<int> result;
+    
+    Number obj1;
+    result = obj1.getLargestNumber({5, 6, 2, 9, 21, 1});
+    
+}

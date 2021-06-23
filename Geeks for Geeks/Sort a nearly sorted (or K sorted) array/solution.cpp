09babbf52052b21270be074be4df985b5ca7749a
Salim <salim7ali@gmail.com>
2021-06-23 22:27:19 +0530

    Solved Medium level problem. Fixed bug issue. Also added a test case.

--- Geeks for Geeks/Sort a nearly sorted (or K sorted) array/solution.cpp
+++ Geeks for Geeks/Sort a nearly sorted (or K sorted) array/solution.cpp
@@ -9,18 +9,19 @@ public:
     vector<int> sortKSortedArray(vector<int> &nums, int k){
         priority_queue<int, vector<int>, greater<int>> min_heap;
 
-        for(int i=0; i<i+k; i++){
+        for(int i=0; i<=0+k; i++){
             min_heap.push(nums[i]);
         }
+        nums[0] = min_heap.top();
+        min_heap.pop();
 
-        for(int i=0; i<nums.size(); i++){
-            int minNum = min_heap.top();
-            min_heap.pop();
-            nums[i] = minNum;
+        for(int i=1; i<nums.size(); i++){
             if(i+k < nums.size())
                 min_heap.push(nums[i+k]);
+            
+            nums[i] = min_heap.top();
+            min_heap.pop();
         }
-
     }
 
     void displayArray(vector<int> nums){
@@ -28,11 +29,15 @@ public:
             cout<<ele<<" ";
         }cout<<"\n";
     }
-}
+};
 
 int main(){
     vector<int> nums = {6, 5, 3, 2, 8, 10, 9};
     Solution obj;
     obj.sortKSortedArray(nums, 3);
+    obj.displayArray(nums);
+
+    nums = {10, 9, 8, 7, 4, 70, 60, 50};
+    obj.sortKSortedArray(nums, 4);
     obj.displayArray(nums);
 }

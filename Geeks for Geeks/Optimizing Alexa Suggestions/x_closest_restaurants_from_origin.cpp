d0206a3ee000e4ce900e1e71c1e65ebfa764e9b6
Salim <salim7ali@gmail.com>
2021-06-24 23:35:40 +0530

    First Commit. Added boiler plate code.

---
+++ Geeks for Geeks/Optimizing Alexa Suggestions/x_closest_restaurants_from_origin.cpp
@@ -0,0 +1,16 @@
+// https://www.geeksforgeeks.org/amazon-interview-experience-13/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Restaurants{
+public:
+    Restaurants(vector<pair<int, int>> location){
+        
+    }
+};
+
+int main(){
+    vector<pair<int, int>> location = {{1, 2}, {3, 4}, {1, -1}};
+    Restaurants obj(location);
+
+}

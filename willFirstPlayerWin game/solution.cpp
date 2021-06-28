7744d3f7bdb9518e70930151c5b675e3ed400db0
Salim <salim7ali@gmail.com>
2021-06-28 23:21:59 +0530

    First commit. Added boiler plate code.

---
+++ willFirstPlayerWin game/solution.cpp
@@ -0,0 +1,20 @@
+// https://www.youtube.com/watch?v=Afnthubp_-M&t=2229s
+/*
+Implement a function willFirstPlaterWin that takes input as a number and returns whether the 
+first player will win the game as per below rules:
+- In each turn, you can decrease the current number from 1 to 9.
+- The player who makes the current number 0 wins the game.
+- Each player plays optimally
+
+Example:
+If curNumber = 23,
+A subtracts 9 => curNumber = 23 - 9 = 14 
+B subtracts 7 => curNumber = 14 - 7 = 7 
+A subtracts 7 and makes 0, so A wins. 
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+}

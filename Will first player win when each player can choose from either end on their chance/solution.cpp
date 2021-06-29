507ada3af6bff563d33b476f9481cd8aad79d339
Salim <salim7ali@gmail.com>
2021-06-29 23:55:06 +0530

    Boiler plate code added.

---
+++ Will first player win when each player can choose from either end on their chance/solution.cpp
@@ -0,0 +1,11 @@
+// https://www.youtube.com/watch?v=Afnthubp_-M&t=1255s
+#include<bits/stdc++.h>
+using namespace std;
+
+bool willFirstPlayerWin(vector<int> nums){
+
+}
+
+int main(){
+    willFirstPlayerWin({3, 2, 5, 5});
+}

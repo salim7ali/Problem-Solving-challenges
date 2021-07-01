7fdf64ac70342d7f9f0ab77331353bb4aee388d0
Salim <salim7ali@gmail.com>
2021-07-01 23:08:04 +0530

    Completed code logic.

--- Will first player win when each player can choose from either end on their chance/solution.cpp
+++ Will first player win when each player can choose from either end on their chance/solution.cpp
@@ -2,10 +2,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool willFirstPlayerWin(vector<int> nums){
+void display(vector<vector<int>> matrix){
+    for(int i=0; i<matrix.size(); i++){
+        for(int j=0; j<matrix.size(); j++){
+            cout<<matrix[i][j]<<" ";
+        }cout<<"\n";
+    }
+}
 
+bool willFirstPlayerWin(vector<int> nums){
+    vector<vector<int>> dp(nums.size(), vector<int> (nums.size(), 0));
+
+    int j=0;
+    for(int outer_i=0; outer_i<dp.size(); outer_i++){
+        for(int i=0; i<dp.size()-outer_i; i++){
+            j = i+outer_i;
+
+            if(i==j){
+                dp[i][i] = nums[i];
+                continue;
+            }
+            dp[i][j] = max(nums[i]+min(dp[i+2][j], dp[i+1][j-1]), nums[j]+min(dp[i][j-2], dp[i+1][j-1]));
+        }
+    }
+    display(dp);
+    return true;
 }
 
 int main(){
-    willFirstPlayerWin({3, 2, 5, 5});
+    cout<<willFirstPlayerWin({3, 2, 5, 5});
 }

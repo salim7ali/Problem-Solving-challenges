f33c5edecd7320b96dd24f679193100a7ec4d2b8
salim <salim7ali@gmail.com>
2021-07-28 14:11:39 +0530

    Created required function.

--- Sorted Array to Balanced BST/solution.cpp
+++ Sorted Array to Balanced BST/solution.cpp
@@ -9,8 +9,23 @@ struct node{
     node *right;
 };
 
+node* createBalancedBST(int L, int R, &nums){
+    if(L==R){
+
+    }
+    
+    int mid = L + floor((R-L)/2);
+    node *currNode = new node();
+    currNode->value = nums[mid];
+
+    currNode->left = createBalancedBST(L, mid-1, nums);
+    currNode->right = createBalancedBST(mid+1, R, nums);
+
+    return currNode;
+}
+
 int main(){
     vector<int> nums = {1, 2, 3, 4};
 
-    node *head = createBalancedBST();
+    node *head = createBalancedBST(0, nums.size()-1, nums);
 }

d1d36c94ba5a9c10db3b7f60106e4f6aaebaa2c5
salim <salim7ali@gmail.com>
2021-07-28 14:26:40 +0530

    Solved medium level problem.

--- Sorted Array to Balanced BST/solution.cpp
+++ Sorted Array to Balanced BST/solution.cpp
@@ -1,4 +1,5 @@
 // Given a sorted array. Write a function that creates a Balanced Binary Search Tree using array elements.
+// https://www.geeksforgeeks.org/sorted-array-to-balanced-bst/
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -9,10 +10,9 @@ struct node{
     node *right;
 };
 
-node* createBalancedBST(int L, int R, &nums){
-    if(L==R){
-
-    }
+node* createBalancedBST(int L, int R, vector<int> &nums){
+    if(L>R)
+        return NULL;
     
     int mid = L + floor((R-L)/2);
     node *currNode = new node();
@@ -24,8 +24,19 @@ node* createBalancedBST(int L, int R, &nums){
     return currNode;
 }
 
+void displayBinaryTree(node *currNode, int level){
+    if(currNode==NULL)
+        return;
+    cout<<currNode->value<<", level: "<<level<<"\n";
+    level +=1;
+    displayBinaryTree(currNode->left, level);
+    displayBinaryTree(currNode->right, level);
+}
+
 int main(){
     vector<int> nums = {1, 2, 3, 4};
 
     node *head = createBalancedBST(0, nums.size()-1, nums);
+    int level = 0;
+    displayBinaryTree(head, level);
 }

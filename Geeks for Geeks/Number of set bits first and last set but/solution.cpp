2cdbcb0ea74235df0f19abd1047ed68191387c90
salim <salim7ali@gmail.com>
2021-07-29 14:14:23 +0530

    First commit. Added boiler plate code.

---
+++ Geeks for Geeks/Number of set bits first and last set but/solution.cpp
@@ -0,0 +1,12 @@
+// Given an integer, find the total number of set bits, first and the last set bit
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class NumberOps{
+public:
+};
+
+int main(){
+  NumberOps obj;  
+}

a784a2931784a33056bfb275477ae972dd1a4f9a
salim <salim7ali@gmail.com>
2021-07-29 14:32:33 +0530

    Added last set bit position function and Solved medium level problem.

--- Geeks for Geeks/Number of set bits first and last set but/solution.cpp
+++ Geeks for Geeks/Number of set bits first and last set but/solution.cpp
@@ -23,15 +23,20 @@ public:
     int getFirstSetBitPosition(){
         return log2(num & ((~num)+1));
     }
+
+    int getLastSetBitPosition(){
+        return log2(num);
+    }
 };
 
 int main(){
   NumberOps obj1(13);  
   cout<<obj1.getNumberOfSetBits()<<"\n";
   cout<<obj1.getFirstSetBitPosition()<<"\n";
-  
+  cout<<obj1.getLastSetBitPosition()<<"\n\n";
 
   NumberOps obj2(64);  
   cout<<obj2.getNumberOfSetBits()<<"\n";
   cout<<obj2.getFirstSetBitPosition()<<"\n";
+  cout<<obj2.getLastSetBitPosition()<<"\n";
 }

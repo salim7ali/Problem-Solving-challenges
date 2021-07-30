0bac970c35ca056b7765d8025ec8e2934723792c
salim <salim7ali@gmail.com>
2021-07-30 18:57:01 +0530

    Completed in order successor function.

--- Inorder successor of given node in BST/inorder_successor.cpp
+++ Inorder successor of given node in BST/inorder_successor.cpp
@@ -3,6 +3,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct Node{
+    int key;
+    Node *left;
+    Node *right;
+    Node *parent;
+
+};
+
+class BinarySearchTree{
+    Node root;
+
+public:
+    Node findInOrderSuccessor(Node inputNode){
+        if(inputNode.right){
+            return firstInorderVal(inputNode.right);
+        }else{
+            Node currNode = inputNode;
+            while(currNode.parent && currNode.parent->right == currNode){
+                currNode = currNode.parent;
+            }
+
+            // If root
+            if(currNode.parent == NULL)
+                return currNode;
+            
+            // Found first parent whose LST we are part of
+            return currNode.parent;
+
+
+        }
+    }
+
+};
+
+
 int main(){
 
 }

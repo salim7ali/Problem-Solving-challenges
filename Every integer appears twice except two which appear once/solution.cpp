9bafce42bcf9765d7ac7cb4e30008eeacb25760b
salim <salim7ali@gmail.com>
2021-08-05 22:18:00 +0530

    uniquePairs function partilly complete.

--- Every integer appears twice except two which appear once/solution.cpp
+++ Every integer appears twice except two which appear once/solution.cpp
@@ -4,10 +4,32 @@
 using namespace std;
 
 class BitOperations{
+    vector<int> nums;
+public:
+    BitOperations(vector<int> nums){
+        this->nums = nums;
+    }
 
+    pair<int, int> uniquePairs(){
+        int fullVectorXOR =0;
+
+        for(int ele: nums){
+            fullVectorXOR ^= ele;
+        }
+        cout<<fullVectorXOR;
+        int firstSetXOR=0;
+        int secondSetXOR=0;
+
+        pair<int, int> uniquePairs;
+        return {1, 2};
+    }
 };
 
 int main(){
     vector<int> nums = {1, 1, 2, 5, 3, 2, 3, 4, 7, 4};
-    BitOperations obj1;
+    pair<int, int> result;
+
+    BitOperations obj1(nums);
+    result = obj1.uniquePairs();
+    
 }

ba9aeae3b0ea0b09b74401b0ee20686885909856
salim <salim7ali@gmail.com>
2021-08-06 23:42:19 +0530

    Solved using shortcut logic trick.

--- XOR_of_all_subsets_given_n_integers/solution.cpp
+++ XOR_of_all_subsets_given_n_integers/solution.cpp
@@ -2,9 +2,20 @@
 using namespace std;
 
 class XOR_Operations{
+    vector<int> nums;
+public:
+    XOR_Operations(vector<int> nums){
+        this->nums = nums;
+    }
+    int xorOfSubsets(){
+        return 0;
+    }
 
 };
 
 int main(){
-    
+    vector<int> nums = {1, 3, 2};
+
+    XOR_Operations obj1(nums);
+    cout<<obj1.xorOfSubsets();
 }

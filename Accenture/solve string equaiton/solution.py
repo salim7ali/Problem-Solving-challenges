3f88e8d2561668cb31cc6091dd38a256436e59c2
salim <salim7ali@gmail.com>
2021-08-19 23:26:19 +0530

    Numbers extracted.

--- Accenture/solve string equaiton/solution.py
+++ Accenture/solve string equaiton/solution.py
@@ -1,5 +1,36 @@
 
 def ReplaceX(s):
+    # case 1
+    if s == None:
+        return -1
+    
+    first_half, second_half = s.split('+')
+    second_half, third_half = second_half.split('=') 
+
+    first_half = first_half.strip(' ')
+    if first_half!='X':
+        first_half = int(first_half.strip(' '))
+
+    second_half = second_half.strip(' ')
+    if second_half!='X':
+        second_half = int(second_half.strip(' '))
+    third_half = third_half.strip(' ')
+    if third_half!='X':
+        third_half = int(third_half.strip(' '))
+
+    print(first_half, second_half, third_half)
+    
+    # for index, singleChar in enumerate(s):
+    #     if(singleChar!='X'):
+    #         continue
+    #     # case 2
+    #     if i==0:
+
+    #     elif s[i-2]=='+':
+
+    #     else:
+
+    #     break
 
 if __name__ == "__main__":
     s = "2 + 15 = X"

28d8f3441b61249f8983542e5be2ebe531ed1d3d
salim <salim7ali@gmail.com>
2021-09-02 18:32:12 +0530

    reverse function completed.

--- Get original list from shifted list given reverse function/solution.cpp
+++ Get original list from shifted list given reverse function/solution.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void reverse(int start, int end, vector<int> &nums){
+    for(int i=start; i<= start+floor((end-start)/2); i++){
+        swap(nums[i], nums[end-(i-start)]);
+    }
+}
+
 void getUnshiftedVector(vector<int> nums, int shiftVal){
     
 }
@@ -8,6 +14,11 @@ void getUnshiftedVector(vector<int> nums, int shiftVal){
 int main(){
     vector<int> nums = {7, 8, 9, 1, 2, 3, 4, 5, 6};
 
-    getUnshiftedVector(nums, 3);
+    // getUnshiftedVector(nums, 3);
+    reverse(3, 8, nums);
+
+    for(int i=0; i<nums.size(); i++){
+        cout<<nums[i]<<" ";
+    }
 
 }

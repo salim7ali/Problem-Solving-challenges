94282646180f818317f44ad98ad72b79d2a0eb82
salim <salim7ali@gmail.com>
2021-09-02 18:21:59 +0530

    Test case added.

---
+++ Get original list from shifted list given reverse function/solution.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void getUnshiftedVector(vector<int> nums, int shiftVal){
+    
+}
+
+int main(){
+    vector<int> nums = {7, 8, 9, 1, 2, 3, 4, 5, 6};
+
+    getUnshiftedVector(nums, 3);
+
+}

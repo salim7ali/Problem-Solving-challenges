3c8e9d8f4eb23089e7f6ed57c9d4bd776af470eb
salim <salim7ali@gmail.com>
2021-09-06 20:10:47 +0530

    Logic complete.

--- Stock span problem in Java/solution.java
+++ Stock span problem in Java/solution.java
@@ -1,5 +1,59 @@
+import java.lang.reflect.InaccessibleObjectException;
+import java.util.Stack;
+
 public class solution {
-    public static void main(String[] args) {
+
+    public static Integer[] stockSpanProblem(Integer[] stocks) {
+        Stack<Integer> stockStack = new Stack<Integer>();
+        Stack<Integer> indexStack = new Stack<Integer>();
+        Stack<Integer> spanStack = new Stack<Integer>();
+    
+        Integer[] result = new Integer[stocks.length];
         
+        
+        stockStack.push(stocks[0]);
+        indexStack.push(0);
+        spanStack.push(1);
+        
+        Integer currSpan = 1;
+        Integer i = 1;
+        while(spanStack.empty() == false && i!=stocks.length){
+            if(stocks[i] < stockStack.peek()){
+                stockStack.push(stocks[i]);
+                indexStack.push(i);
+                spanStack.push(1);
+                i += 1;
+                continue;
+            }
+            while(stocks[i] > stockStack.peek()){
+                currSpan += spanStack.peek();
+                result[indexStack.peek()] = spanStack.peek();
+                
+                spanStack.pop();
+                indexStack.pop();
+                stockStack.pop();
+            }
+            
+            stockStack.push(stocks[i]);
+            indexStack.push(i);
+            spanStack.push(currSpan);
+            currSpan =1;
+            i+=1;
+        }
+        while(spanStack.empty() == false){
+            result[indexStack.peek()] = spanStack.peek();
+            
+            spanStack.pop();
+            indexStack.pop();
+            stockStack.pop();
+
+        }
+
+
+        return result;
+    }
+    public static void main(String[] args) {
+        Integer[] stocks = {100, 80,60, 60, 70, 60, 75, 85};
+        System.out.println(stockSpanProblem(stocks));
     }
 }

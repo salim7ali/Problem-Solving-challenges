c13f3e15fffbd085e019ff46bcadd314d32763d6
salim <salim7ali@gmail.com>
2021-10-07 20:37:54 +0530

    Function to find the pivot point coded.

---
+++ Geeks for Geeks/Find element in sorted rotated array/find_ele_in_sorted_rot_array.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findPivot(vector<int> &nums){
+    
+}
+
+int main(){
+    vector<int> nums = {5, 6, 7, 8, 9, 10, 1, 2, 3};
+    
+    int pivot = findPivot(nums);
+}

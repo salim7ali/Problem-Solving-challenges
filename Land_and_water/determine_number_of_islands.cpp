a3490016fc0b0eef04a4a8256add6e922a96497b
salim7ali <salim7ali@gmail.com>
2023-05-15 23:52:21 +0530

    Boiler plate code

---
+++ Land_and_water/determine_number_of_islands.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class determine_number_of_islands
+{
+private:
+    /* data */
+public:
+    determine_number_of_islands(/* args */);
+    ~determine_number_of_islands();
+};
+
+int main(){
+    
+    
+    return 0;
+}

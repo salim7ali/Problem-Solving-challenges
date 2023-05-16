cdadc462dbe52b37e25ba5ee3fe9def639edb696
salim7ali <salim7ali@gmail.com>
2023-05-16 20:37:10 +0530

    Solved

--- generate_all_subsequences_of_array/solution.cpp
+++ generate_all_subsequences_of_array/solution.cpp
@@ -1,12 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void generateAllSubsequences(vector<int> &nums){
-    
+void printVector(vector<int> currSubequnce){
+    for(int i=0; i<currSubequnce.size(); i++){
+        cout<<currSubequnce[i]<<" ";
+    }cout<<"\n";
+}
+
+void generateAllSubsequences(vector<int> &nums, int currIndex, vector<int> currSubequnce){
+    if(currIndex==nums.size()){
+        printVector(currSubequnce);
+        return;
+    }
+    vector<int> pickedSubsequence = currSubequnce;
+    pickedSubsequence.push_back(nums[currIndex]);
+    generateAllSubsequences(nums, currIndex+1, pickedSubsequence);   // pick
+    generateAllSubsequences(nums, currIndex+1, currSubequnce);   // dont pick
 }
 
 int main(){
     vector<int> nums = {1, 7, 11};
 
-    generateAllSubsequences(nums);
+    generateAllSubsequences(nums, 0, {});
 }

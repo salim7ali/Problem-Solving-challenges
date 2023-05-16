eb67becdda559a16302b4266083b16af6121d8f9
salim7ali <salim7ali@gmail.com>
2023-05-16 20:19:58 +0530

    Boiler plate code

---
+++ generate_all_subsequences_of_array/solution.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void generateAllSubsequences(vector<int> &nums){
+    
+}
+
+int main(){
+    vector<int> nums = {1, 7, 11};
+
+    generateAllSubsequences(nums);
+}

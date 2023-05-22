e52a981b0641a3cf15cf6eacbb8b1914e07f3ec1
salim7ali <salim7ali@gmail.com>
2023-05-22 23:28:56 +0530

    Node class added

--- Print_left_view_of_binary_tree/solution.cpp
+++ Print_left_view_of_binary_tree/solution.cpp
@@ -1,6 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Node{
+    int value;
+    Node *left, *right;
+public:
+    Node(){
+        this->left = NULL;
+        this->right = NULL;
+    }
+
+    void setLeft(Node *left){
+        this->left = left;
+    }
+
+    void setRight(Node *right){
+        this->right = right;
+    }
+
+}; 
+
+void printLeftView(Node* currNode){
+    queue<pair<Node*, int>> bfsQueue;
+    bfsQueue.push({currNode, 1});
+
+    
+}
+
 int main(){
     
     

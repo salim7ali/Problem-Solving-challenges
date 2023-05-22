9bd53e5e2aed7b67a85eec32d0feccde1b68423d
salim7ali <salim7ali@gmail.com>
2023-05-22 23:53:15 +0530

    Solved

--- Print_left_view_of_binary_tree/solution.cpp
+++ Print_left_view_of_binary_tree/solution.cpp
@@ -17,13 +17,37 @@ public:
     void setRight(Node *right){
         this->right = right;
     }
+    int getValue(){
+        return this->value;
+    }
+    Node* getLeft(){
+        return this->left;
+    }
+    Node* getRight(){
+        return this->right;
+    }
 
 }; 
 
-void printLeftView(Node* currNode){
+void printLeftView(Node* parentNode){
     queue<pair<Node*, int>> bfsQueue;
-    bfsQueue.push({currNode, 1});
+    bfsQueue.push({parentNode, 1});
 
+    pair<Node*, int> currNodeAndLevel;
+    int levelNeeded = 1;
+    while (bfsQueue.empty() == false){
+        currNodeAndLevel = bfsQueue.front();
+        bfsQueue.pop();
+
+        if(currNodeAndLevel.second == levelNeeded){
+            cout<<currNodeAndLevel.first->getValue();
+            levelNeeded +=1;
+        }
+
+        Node* currNode = currNodeAndLevel.first;
+        bfsQueue.push({currNode->getLeft(), currNodeAndLevel.second+1});
+        bfsQueue.push({currNode->getRight() , currNodeAndLevel.second+1});
+    }
     
 }
 

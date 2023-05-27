ae818e5efccc4137bf10104045864d943dd819c1
salim7ali <salim7ali@gmail.com>
2023-05-27 23:48:50 +0530

    Solved

--- Reverse_singly_linked_list/solution.cpp
+++ Reverse_singly_linked_list/solution.cpp
@@ -1,4 +1,33 @@
+// https://leetcode.com/problems/reverse-linked-list/
 #include<bits/stdc++.h>
-int main(){
+using namespace std;
 
-}
+//  * Definition for singly-linked list.
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+public:
+    void recursiveCall(ListNode* prevNode, ListNode* currNode, ListNode* head){
+        // no node edge case
+        if(currNode == NULL)
+            return;
+        if(currNode->next == NULL){
+            recursiveCall(currNode, currNode->next, head);
+        }else{  //  reached tail
+            head = currNode;
+        }
+
+        currNode->next = prevNode;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        recursiveCall(NULL, head, head);
+        return head;
+    }
+};
